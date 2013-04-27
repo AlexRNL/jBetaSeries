8128911d7704390ab747cab7f6b0254aa8384965
Alex Barféty <alexbarfety@free.fr>
2013-04-27 21:29:15 +0200

    Add constructors to main class

--- src/main/java/com/alexrnl/jbetaseries/JBetaSeries.java
+++ src/main/java/com/alexrnl/jbetaseries/JBetaSeries.java
@@ -6,14 +6,39 @@ import java.util.logging.Logger;
  * Entry point of the interface to the BetaSeries API.<br />
  * @author Alex
  */
-public class JBetaSeries {
+public final class JBetaSeries {
 	/** Logger */
 	private static Logger	lg	= Logger.getLogger(JBetaSeries.class.getName());
 	
 	/** The key to use for the API */
-	private String key;
+	private final String	key;
 	/** The preferred data type format */
-	private Format preferredFormat;
-	/** The token for the user */
-	private String token;
+	private final Format	format;
+	/** The token for the user, <code>null</code> if no user is logged. */
+	private final String	token;
+	
+	/**
+	 * Constructor #1.<br />
+	 * This will use the {@link Format#XML XML} format of the API.
+	 * @param key
+	 *        the API key to use.
+	 */
+	public JBetaSeries (final String key) {
+		this(key, Format.XML);
+	}
+	
+	/**
+	 * Constructor #2.<br />
+	 * @param key
+	 *        the API key to use.
+	 * @param format
+	 *        the data type required for the API.
+	 */
+	public JBetaSeries (final String key, final Format format) {
+		super();
+		this.key = key;
+		this.format = format;
+		this.token = null;
+	}
+	
 }
