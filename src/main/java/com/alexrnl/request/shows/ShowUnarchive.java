9c73b9e29360aea72005c3954304479658313173
Alex Barféty <alexbarfety@free.fr>
2013-04-28 13:10:18 +0200

    Add request for unarchiving show

---
+++ src/main/java/com/alexrnl/request/shows/ShowUnarchive.java
@@ -0,0 +1,48 @@
+package com.alexrnl.request.shows;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alexrnl.request.Request;
+import com.alexrnl.request.Verb;
+
+/**
+ * Archive a show in the logged member account.<br />
+ * @author Alex
+ */
+public class ShowUnarchive extends Request {
+	
+	/** Address of the method */
+	private static final String	ADDRESS			= "/shows/archive";
+	/** Name of the id parameter */
+	private static final String	PARAMETER_ID	= "id";
+	
+	/** The id of the show to archive */
+	private final Integer		id;
+	
+	/**
+	 * Constructor #1.<br />
+	 * @param id
+	 *        the id of the show to archive.
+	 */
+	public ShowUnarchive (final Integer id) {
+		super(Verb.DELETE, ADDRESS);
+		this.id = id;
+	}
+	
+	/**
+	 * Return the attribute id.
+	 * @return the attribute id.
+	 */
+	public Integer getId () {
+		return id;
+	}
+	
+	@Override
+	public Map<String, String> getParameters () {
+		final Map<String, String> parameters = new HashMap<>();
+		parameters.put(PARAMETER_ID, getId().toString());
+		return parameters;
+	}
+	
+}
