dd6c17616cda248a835b97c253fa8a3ef01d2d3a
Alex Barféty <alexbarfety@free.fr>
2013-04-28 16:58:44 +0200

    Adding new parameter ShowIds

---
+++ src/main/java/com/alexrnl/jbetaseries/request/parameters/ShowIds.java
@@ -0,0 +1,20 @@
+package com.alexrnl.jbetaseries.request.parameters;
+
+import com.alexrnl.commons.utils.StringUtils;
+
+/**
+ * Parameter which allow to set several ids of shows.<br />
+ * @author Alex
+ */
+public class ShowIds extends Parameter<String> {
+	
+	/**
+	 * Constructor #1.<br />
+	 * @param ids
+	 *        the id of the show.
+	 */
+	public ShowIds (final Integer... ids) {
+		super(ShowId.PARAMETER_ID, StringUtils.separateWith(",", (Object[]) ids));
+	}
+	
+}
