04e5c353b25f2c3edb41544f35f7f40b3903e4df
Alex Barféty <alexbarfety@free.fr>
2013-05-11 02:17:29 +0200

    Fix spelling

--- src/main/java/com/alexrnl/jbetaseries/request/members/package-info.java
+++ src/main/java/com/alexrnl/jbetaseries/request/members/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Package for the requests regarding the members.<br />
- * The associated documentation for these method is available at
+ * The associated documentation for these methods is available at
  * <a href="http://www.betaseries.com/api/methodes/members">this address</a>.
  * @author Alex
  */
--- src/main/java/com/alexrnl/jbetaseries/request/shows/package-info.java
+++ src/main/java/com/alexrnl/jbetaseries/request/shows/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Package for the requests regarding the shows.<br />
- * The associated documentation for these method is available at
+ * The associated documentation for these methods is available at
  * <a href="http://www.betaseries.com/api/methodes/shows">this address</a>.
  * @author Alex
  */
