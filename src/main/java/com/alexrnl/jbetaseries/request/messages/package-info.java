128c43b5dee0bdc195d0bc1a0f402d45b37da832
Alex Barféty <alexbarfety@free.fr>
2013-06-11 00:18:19 +0200

    Add the package for the messages related requests

---
+++ src/main/java/com/alexrnl/jbetaseries/request/messages/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for the requests regarding the messages.<br />
+ * The associated documentation for these methods is available at
+ * <a href="http://www.betaseries.com/api/methodes/messages">this address</a>.
+ * @author Alex
+ */
+package com.alexrnl.jbetaseries.request.messages;
