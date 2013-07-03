509a947d29fec4d45058ff96aea3c8302318bf4b
Alex Barféty <alexbarfety@free.fr>
2013-07-03 23:29:20 +0200

    Add test suite for the Verb class

--- src/test/java/com/alexrnl/jbetaseries/request/RequestTests.java
+++ src/test/java/com/alexrnl/jbetaseries/request/RequestTests.java
@@ -9,7 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * @author Alex
  */
 @RunWith(Suite.class)
-@SuiteClasses({ FormatTest.class })
+@SuiteClasses({ FormatTest.class, VerbTest.class })
 public class RequestTests {
 	
 }
---
+++ src/test/java/com/alexrnl/jbetaseries/request/VerbTest.java
@@ -0,0 +1,23 @@
+package com.alexrnl.jbetaseries.request;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test suite for the {@link Verb} class.
+ * @author Alex
+ */
+public class VerbTest {
+	
+	/**
+	 * Test method for {@link com.alexrnl.jbetaseries.request.Verb#getHttpMethodName()}.
+	 */
+	@Test
+	public void testGetHttpMethodName () {
+		assertEquals("GET", Verb.GET.getHttpMethodName());
+		assertEquals("POST", Verb.POST.getHttpMethodName());
+		assertEquals("PUT", Verb.PUT.getHttpMethodName());
+		assertEquals("DELETE", Verb.DELETE.getHttpMethodName());
+	}
+}
