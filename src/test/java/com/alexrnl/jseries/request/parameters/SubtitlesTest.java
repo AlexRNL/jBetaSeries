7a04145519b217064355417c68c417c9ad2a8c88
Alex Barféty <alexbarfety@free.fr>
2013-10-16 23:04:00 +0200

    Add test suite for the Subtitles class

---
+++ src/test/java/com/alexrnl/jseries/request/parameters/SubtitlesTest.java
@@ -0,0 +1,71 @@
+package com.alexrnl.jseries.request.parameters;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.alexrnl.jseries.request.parameters.Subtitles.Filter;
+
+/**
+ * Test suite for the {@link Subtitles} class.
+ * @author Alex
+ */
+public class SubtitlesTest {
+	/** The all subtitle parameter to test */
+	private Subtitles	subtitleAll;
+	/** The VO subtitle parameter to test */
+	private Subtitles	subtitleVO;
+	/** The VF subtitle parameter to test */
+	private Subtitles	subtitleVF;
+	/** The VOVF subtitle parameter to test */
+	private Subtitles	subtitleVOVF;
+	
+	/**
+	 * Set up test attributes.
+	 */
+	@Before
+	public void setUp () {
+		subtitleAll = new Subtitles(Filter.ALL);
+		subtitleVO = new Subtitles(Filter.VO);
+		subtitleVF = new Subtitles(Filter.VF);
+		subtitleVOVF = new Subtitles(Filter.VOVF);
+	}
+	
+	/**
+	 * Test method for {@link Parameter#getName()}.
+	 */
+	@Test
+	public void testGetName () {
+		assertEquals("subtitles", subtitleAll.getName());
+		assertEquals("subtitles", subtitleVO.getName());
+		assertEquals("subtitles", subtitleVF.getName());
+		assertEquals("subtitles", subtitleVOVF.getName());
+	}
+	
+	/**
+	 * Test method for {@link Parameter#getValue()}.
+	 */
+	@Test
+	public void testGetValue () {
+		assertEquals(Filter.ALL, subtitleAll.getValue());
+		assertEquals(Filter.VO, subtitleVO.getValue());
+		assertEquals(Filter.VF, subtitleVF.getValue());
+		assertEquals(Filter.VOVF, subtitleVOVF.getValue());
+	}
+	
+	/**
+	 * Test method for the {@link Filter} enumeration.
+	 */
+	@Test
+	public void testSubtitlesFilterEnum () {
+		assertEquals("all", Filter.ALL.getValue());
+		assertEquals("vo", Filter.VO.getValue());
+		assertEquals("vf", Filter.VF.getValue());
+		assertEquals("vovf", Filter.VOVF.getValue());
+		assertEquals("all", Filter.ALL.toString());
+		assertEquals("vo", Filter.VO.toString());
+		assertEquals("vf", Filter.VF.toString());
+		assertEquals("vovf", Filter.VOVF.toString());
+	}
+}
