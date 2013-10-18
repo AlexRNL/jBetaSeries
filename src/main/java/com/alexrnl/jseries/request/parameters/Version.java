92fe72cc4b804e6383e88ad034582c8b1564ae03
Alex Barféty <alexbarfety@free.fr>
2013-10-18 23:57:48 +0200

    Update parameter version constant names

--- src/main/java/com/alexrnl/jseries/request/parameters/Version.java
+++ src/main/java/com/alexrnl/jseries/request/parameters/Version.java
@@ -1,20 +1,19 @@
 package com.alexrnl.jseries.request.parameters;
 
-
 /**
  * Parameter indicating to use the new version of the BetaSeries API.<br />
  * @author Alex
  */
 public class Version extends Parameter<String> {
 	/** Name of the beta parameter */
-	private static final String	PARAMETER_BETA	= "v";
+	private static final String	PARAMETER_VERSION	= "v";
 	/** Version of the API to use TODO parametrize */
-	private static final String	BETA_VERSION	= "2.1";
+	private static final String	VERSION_NUMBER		= "2.1";
 	
 	/**
 	 * Constructor #1.<br />
 	 */
 	public Version () {
-		super(PARAMETER_BETA, BETA_VERSION);
+		super(PARAMETER_VERSION, VERSION_NUMBER);
 	}
 }
