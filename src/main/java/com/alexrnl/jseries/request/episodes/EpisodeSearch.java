81b532e4fd8e3d3fc5aea90470b5962ee7efb607
Alex Barféty <alexbarfety@free.fr>
2015-03-21 20:35:33 +0100

    Add unit test of episodes/search request and the subtitle parameter

--- src/main/java/com/alexrnl/jseries/request/episodes/EpisodeSearch.java
+++ src/main/java/com/alexrnl/jseries/request/episodes/EpisodeSearch.java
@@ -5,6 +5,7 @@ import com.alexrnl.jseries.request.Request;
 import com.alexrnl.jseries.request.Verb;
 import com.alexrnl.jseries.request.parameters.NumberStr;
 import com.alexrnl.jseries.request.parameters.ShowId;
+import com.alexrnl.jseries.request.parameters.Subtitles;
 
 /**
  * Request for searching for a specific episode, using its show id and it number in the series.<br />
@@ -18,11 +19,16 @@ public class EpisodeSearch extends Request {
 	 *        the id of the show.
 	 * @param episodeString
 	 *        the number of the episode, of the form <code>SXXEXX</code>.
+	 * @param subtitles
+	 *        if <code>true</code> request the subtitles of the episode.
 	 */
-	public EpisodeSearch (final Integer showId, final String episodeString) {
+	public EpisodeSearch (final Integer showId, final String episodeString, final Boolean subtitles) {
 		super(Verb.GET, APIAddresses.EPISODES_SEARCH);
 		addParameter(new ShowId(showId));
 		addParameter(new NumberStr(episodeString));
+		if (subtitles) {
+			addParameter(new Subtitles());
+		}
 	}
 	
 	/**
@@ -31,8 +37,32 @@ public class EpisodeSearch extends Request {
 	 *        the id of the show.
 	 * @param globalNumber
 	 *        the global number of the episode.
+	 * @param subtitles
+	 *        if <code>true</code> request the subtitles of the episode.
+	 */
+	public EpisodeSearch (final Integer showId, final Integer globalNumber, final Boolean subtitles) {
+		this(showId, globalNumber.toString(), subtitles);
+	}
+	
+	/**
+	 * Constructor #3.<br />
+	 * @param showId
+	 *        the id of the show.
+	 * @param episodeString
+	 *        the number of the episode, of the form <code>SXXEXX</code>.
+	 */
+	public EpisodeSearch (final Integer showId, final String episodeString) {
+		this(showId, episodeString, false);
+	}
+	
+	/**
+	 * Constructor #4.<br />
+	 * @param showId
+	 *        the id of the show.
+	 * @param globalNumber
+	 *        the global number of the episode.
 	 */
 	public EpisodeSearch (final Integer showId, final Integer globalNumber) {
-		this(showId, globalNumber.toString());
+		this(showId, globalNumber, false);
 	}
 }
---
+++ src/test/java/com/alexrnl/jseries/request/episodes/EpisodeSearchTest.java
@@ -0,0 +1,68 @@
+package com.alexrnl.jseries.request.episodes;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.alexrnl.jseries.request.Request;
+import com.alexrnl.jseries.request.Verb;
+import com.alexrnl.jseries.request.parameters.NumberStr;
+import com.alexrnl.jseries.request.parameters.ShowId;
+import com.alexrnl.jseries.request.parameters.Subtitles;
+import com.alexrnl.jseries.request.parameters.Version;
+
+/**
+ * Test suite for the {@link EpisodeSearch} class.
+ * @author Alex
+ */
+public class EpisodeSearchTest {
+	/** Episode search request */
+	private EpisodeSearch	episodeSearch;
+	/** Episode search request with a global number*/
+	private EpisodeSearch	episodeSearchGlobal;
+	/** Episode search request with subtitle */
+	private EpisodeSearch	episodeSearchSubtitle;
+	
+	/**
+	 * Set up test attributes.
+	 */
+	@Before
+	public void setUp () {
+		episodeSearch = new EpisodeSearch(3315, "s04e11");
+		episodeSearchGlobal = new EpisodeSearch(3315, 79);
+		episodeSearchSubtitle = new EpisodeSearch(3315, "s04e11", true);
+	}
+	
+	/**
+	 * Test method for {@link Request#getVerb()}.
+	 */
+	@Test
+	public void testGetVerb () {
+		assertEquals(Verb.GET, episodeSearch.getVerb());
+		assertEquals(Verb.GET, episodeSearchGlobal.getVerb());
+		assertEquals(Verb.GET, episodeSearchSubtitle.getVerb());
+	}
+	
+	/**
+	 * Test method for {@link Request#getMethod()}.
+	 */
+	@Test
+	public void testGetMethod () {
+		assertEquals("/episodes/search", episodeSearch.getMethod());
+		assertEquals("/episodes/search", episodeSearchGlobal.getMethod());
+		assertEquals("/episodes/search", episodeSearchSubtitle.getMethod());
+	}
+	
+	/**
+	 * Test method for {@link Request#getParameters()}.
+	 */
+	@Test
+	public void testGetParameters () {
+		assertEquals(Arrays.asList(new Version(), new ShowId(3315), new NumberStr("s04e11")), episodeSearch.getParameters());
+		assertEquals(Arrays.asList(new Version(), new ShowId(3315), new NumberStr("79")), episodeSearchGlobal.getParameters());
+		assertEquals(Arrays.asList(new Version(), new ShowId(3315), new NumberStr("s04e11"), new Subtitles()), episodeSearchSubtitle.getParameters());
+	}
+}
