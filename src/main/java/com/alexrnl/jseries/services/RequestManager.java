ab6bb7c49fa19be783be79ffed3c8b78d3ec2e53
Alex Barféty <alexbarfety@free.fr>
2015-03-27 01:17:22 +0100

    Extract constant from RequestManager class

--- src/main/java/com/alexrnl/jseries/services/RequestManager.java
+++ src/main/java/com/alexrnl/jseries/services/RequestManager.java
@@ -7,13 +7,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.alexrnl.commons.error.ExceptionUtils;
 import com.alexrnl.jseries.request.APIAddresses;
 import com.alexrnl.jseries.request.Request;
 import com.alexrnl.jseries.request.Verb;
@@ -24,13 +22,24 @@ import com.alexrnl.jseries.request.parameters.Parameter;
  * @author Alex
  */
 public class RequestManager {
+
 	/** Logger */
 	private static final Logger				LG	= Logger.getLogger(RequestManager.class.getName());
 	
-	/** The name for the API key parameter */
-	public static final String	KEY_PARAMETER						= "X-BetaSeries-Key";
-	/** The name for the API token parameter */
-	public static final String	TOKEN_PARAMETER						= "X-BetaSeries-Token";
+	// Standard HTTP properties
+	/** The name for the content length property */
+	private static final String				PROPERTY_CONTENT_LENGTH	= "Content-Length";
+	/** The name for the accept property */
+	private static final String				PROPERTY_ACCEPT			= "Accept";
+	/** The name for the user-agent property */
+	private static final String				PROPERTY_USER_AGENT		= "User-Agent";
+	/** The name for the accept charset property */
+	private static final String				PROPERTY_ACCEPT_CHARSET	= "Accept-Charset";
+	// BetaSeries properties
+	/** The name for the API key property */
+	public static final String				PROPERTY_KEY			= "X-BetaSeries-Key";
+	/** The name for the API token property */
+	public static final String				PROPERTY_TOKEN			= "X-BetaSeries-Token";
 	
 	/** The configuration of the connector */
 	private final Configuration				configuration;
@@ -75,42 +84,37 @@ public class RequestManager {
 		}
 		
 		// Open connection to address
-		try {
-			if (LG.isLoggable(Level.INFO)) {
-				LG.info("Connecting to " + address);
-			}
-			connection = httpConnectionProvider.getHttpConnection(address.toString());
-		} catch (final MalformedURLException e) {
-			LG.warning("Could not build URL: " + ExceptionUtils.display(e));
-			throw new IOException("Could not build address from " + address.toString(), e);
+		if (LG.isLoggable(Level.INFO)) {
+			LG.info("Connecting to " + address);
 		}
+		connection = httpConnectionProvider.getHttpConnection(address.toString());
 		
 		// Set connection properties
-		connection.setDoOutput(true);
+		connection.setDoOutput(isPost(request));
 		connection.setDoInput(true);
 		connection.setRequestMethod(request.getVerb().getHttpMethodName());
-		connection.addRequestProperty("Accept-Charset", configuration.getCharset().name());
-		connection.addRequestProperty("User-Agent", configuration.getUserAgent());
-		connection.addRequestProperty("Accept", configuration.getFormat().getDescription());
-		connection.addRequestProperty(KEY_PARAMETER, configuration.getKey());
+		connection.addRequestProperty(PROPERTY_ACCEPT_CHARSET, configuration.getCharset().name());
+		connection.addRequestProperty(PROPERTY_USER_AGENT, configuration.getUserAgent());
+		connection.addRequestProperty(PROPERTY_ACCEPT, configuration.getFormat().getDescription());
+		connection.addRequestProperty(PROPERTY_KEY, configuration.getKey());
 		if (token != null) {
-			connection.addRequestProperty(TOKEN_PARAMETER, token);
+			connection.addRequestProperty(PROPERTY_TOKEN, token);
 		}
 		
 		// Writing parameter for post request
 		if (isPost(request)) {
 			final byte[] parameterBytes = parameters.getBytes(configuration.getCharset());
 			connection.setInstanceFollowRedirects(false);
-			connection.setRequestProperty("Content-Length", String.valueOf(parameterBytes.length));
+			connection.addRequestProperty(PROPERTY_CONTENT_LENGTH, String.valueOf(parameterBytes.length));
 			if (LG.isLoggable(Level.INFO)) {
 				LG.info("Sending parameters: " + parameters);
 			}
 			
 			connection.connect();
-			final OutputStream wr = connection.getOutputStream();
-			wr.write(parameterBytes);
-			wr.flush();
-			wr.close();
+			try (final OutputStream wr = connection.getOutputStream()) {
+				wr.write(parameterBytes);
+				wr.flush();
+			}
 		} else {
 			connection.connect();
 		}
@@ -126,10 +130,8 @@ public class RequestManager {
 		try (final BufferedReader rd = new BufferedReader(new InputStreamReader(stream, configuration.getCharset()))) {
 			String line;
 			while ((line = rd.readLine()) != null) {
-				sb.append(line + '\n');
+				sb.append(line + System.lineSeparator());
 			}
-		} catch (final IOException e) {
-			LG.warning("Error while reading data from API: " + ExceptionUtils.display(e));
 		}
 		return sb.toString();
 	}
